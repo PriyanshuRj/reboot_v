8b649dbc1669ef5795705e5c7294d30533388456
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-02-17 10:29:27 +0530

    Initialized gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+      title: `Vibhav`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [ "gatsby-plugin-react-helmet", "gatsby-plugin-mdx",  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
---
+++ src/pages/team/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TeamPage() {
+  return (
+    <div > our team page</div>
+  )
+}
